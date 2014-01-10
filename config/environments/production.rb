faa7bd11ad6750d09f6738a42dbb1213b46b2714
Alexander Andreev <cinic.rus@gmail.com>
2014-01-10 19:17:44 +0400

    поправочки в конфиг компиляции шрифта

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ OpenBroker::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['admin/admin.js', 'admin/admin.css', 'application/application.js', 'application/application.css']
+  config.assets.precompile += ['admin/admin.js', 'admin/admin.css', 'application/application.js', 'application/application.css', 'fonts/fonts.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
