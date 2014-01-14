c21c09f4ef84fd3c6f31273a2d758bc599429dfa
Aleksandr <andreev-a@open.ru>
2014-01-14 13:05:37 +0400

    Обновлена высота блоков и настройка

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,7 +1,7 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_mongoid_attached_file :data,
-                            :url => "/uploads/attachments/:id/:filename",
-                            :path => ":rails_root/public/uploads/attachments/:id/:filename"
+                            :url => "/upload/attachments/:id/:filename",
+                            :path => ":rails_root/public/upload/attachments/:id/:filename"
 
   validates_attachment_size :data, :less_than => 10.megabytes
   validates_attachment_presence :data
