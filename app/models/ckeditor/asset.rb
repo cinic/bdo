17866a0fe54044d82ea6f315a8b464b4e6a00ec9
Aleksandr <andreev-a@open.ru>
2014-01-21 18:29:20 +0400

    Обновил модель CKEditor
    Для хранения графики и файлов на сервере со статикой добавил адрес
    сервера статики из конфига

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -2,4 +2,8 @@ class Ckeditor::Asset
   include Ckeditor::Orm::Mongoid::AssetBase
   include Mongoid::Paperclip
   include Ckeditor::Backend::Paperclip
+
+  def self.server_url
+    OpenBroker::Application.config.action_controller.asset_host if Rails.env.production?
+  end
 end
