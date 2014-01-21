31a9cd3325d2a4c8d2d700b1c015ef2a56b619ea
Aleksandr <andreev-a@open.ru>
2014-01-21 16:43:21 +0400

    Test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ OpenBroker::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['admin/admin', 'application/application', 'fonts']
+  config.assets.precompile += ['admin', 'application', 'fonts']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
