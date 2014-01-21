91b4c698ee6ec0923393315417c2fa177a108947
Aleksandr <andreev-a@open.ru>
2014-01-21 16:08:11 +0400

    Правки CKeditor add BaseHref

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -8,6 +8,7 @@ CKEDITOR.editorConfig = function( config )
         /*,
         { name: 'links', items : [ 'Link','Unlink','Anchor' ] }*/
     ];
+    config.baseHref = 'https://static.bdo.ifoxy.net/assets/ckeditor/';
     config.coreStyles_bold = { element : 'b', overrides : 'strong' };
     config.coreStyles_italic = { element : 'i', overrides : 'em' };
     config.pasteFromWordRemoveFontStyles = true;
