f2dd6fe050981726407df9bcce820ebd0a79e6e7
Aleksandr <andreev-a@open.ru>
2014-03-28 14:11:31 +0400

    Изменил параметр use_sudo в тру

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/upload}
 
 set :default_env, { rvm_bin_path: '~/.rvm/bin' }
 set :keep_releases, 3
-#set :use_sudo, false
+set :use_sudo, true
 
 namespace :deploy do
   desc "Export the Procfile to Ubuntu's upstart scripts"
