1a83f7d1d0d9ad6d6d4ea55048c2819f5770e448
Aleksandr <andreev-a@open.ru>
2014-04-10 14:01:49 +0400

    Обновил модель и тестовую wsdl

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -24,81 +24,81 @@ class Prospect
   validates :patronymic, format: { with: /[[:alpha:]]/ }, allow_blank: true
   validates :mobile, presence: true
 
-  private
+  def send_to_clients(params = {}, procedure, anketa_type)
+    client = TinyTds::Client.new(:username => 'phpuser', :password => 'phpuser', :host => '10.48.34.220', :database => "opendb", :port => "1433")
 
-  def self.service_params
-    {
-      :id => "NEW",
-      :prospectSourceType => "Our own sales network", 
-      :"web-RequestType" => "Active Sale",
-      :lastName => "Test",
-      :firstName => "Test",
-      :middleName => "Test",
-      :alternatePhone => "+79119998877",
-      :emailAddress => "andreev-a@open.ru",
-      :comment => "Тестирование WSDL",
-      :bestCallTime => "",
-      :callbackDatetime => "",
-      :city => "Москва",
-      :comments => "Тестирование WSDL",
-      :organization => "",
-      :"web-RequestSourceURL" => "HTTP RequestSourceURL",
-      :"web-RequestHTTPReferer" => "HTTP RequestHTTPReferer",
-      :formSourceURL => "formSourceURL",
-      :"web-RequestProductName" => "Имя продукта"
-    }
+    names = "uch1,uch2,uch3,uch4,uch5,uch6,uch7,uch8,uch9,uch10,uch11,uch12,uch13,uch14,uch15,uch16,uch17"
+    values = "''#{params['last_name']}'',''#{params['first_name']}'','''','''',''#{params['city']}'','''',''#{params['mobile']}'',''#{params['email']}'',''1'',''0'',''1'',''0'','''',''1'',''1'',''0'',''http://localhost:3000''"
+
+    query = "EXEC #{procedure} @anketa_type ='#{anketa_type}' ,@anketa_names ='#{names}' ,@anketa_values ='#{values}' ,@is_insert = 1"
+    
+    result = client.execute(query)
+    client.close
   end
-  
-  def self.send_to_crm(params = {})
+
+  def send_to_crm(params = {})
     data = {
       :id => "NEW",
       :prospectSourceType => "Our own sales network", 
-      :"web-RequestType" => params[:request_type],
-      :lastName => params[:last_name],
-      :firstName => params[:first_name],
-      :middleName => params[:patronymic],
-      :alternatePhone => params[:mobile],
-      :emailAddress => params[:email],
+      :"web-RequestType" => params["request_type"],
+      :lastName => params["last_name"],
+      :firstName => params["first_name"],
+      :middleName => params["patronymic"],
+      :alternatePhone => params["mobile"],
+      :emailAddress => params["email"],
       :comment => "",
       :bestCallTime => "",
       :callbackDatetime => "",
       :city => params[:city],
       :comments => "Тестирование WSDL",
       :organization => "",
-      :"web-RequestSourceURL" => params[:first_requested_page],
-      :"web-RequestHTTPReferer" => params[:referer],
-      :formSourceURL => params[:form_url],
-      :"web-RequestProductName" => params[:product_name]
+      :"web-RequestSourceURL" => params["first_requested_page"],
+      :"web-RequestHTTPReferer" => params["referer"],
+      :formSourceURL => params["form_url"],
+      :"web-RequestProductName" => params["product"]
     }
 
-    path = File.join(Rails.root, "config", "crm", "prospect.wsdl")
+    path = File.join(Rails.root, "config", "crm", "prospect.test1.wsdl")
     client = Savon.new(path)
+
     operation = client.operation(:OPN_spceSales_spcProspective_spcContact_spcWebsite_spcImport, :OPN_spceSales_spcProspective_spcContact_spcWebsite_spcImport, :ProspectiveContactWebsiteImport)
     operation.body = {:Pospective_spcContact => {:listOfeSalesProspectiveContact => {:listMgmtProspectiveContact => [data]}}}
 
     response = operation.call 
-    id = response.body[:prospective_contact_website_import_response][:prospective_spc_contact_spc_insert_spc_id]
+    #id = response.body[:prospective_contact_website_import_response][:prospective_spc_contact_spc_insert_spc_id]
 
-    case params[:request_type]
+    case params["request_type"]
     when "Join Quik Demo"
-      params[:anketa_type] = "saturn.ChildrenDB.dbo.ChReg"
-      send_to_clients(params)
+      clients_result = send_to_clients(params, "dbo.lk_anketa_handler", "saturn.ChildrenDB.dbo.ChReg")
+    when "Juridical Affiliate Contact"
+      clients_result = send_to_clients(params, "dbo.lk_anketa_handler", "saturn.ChildrenDB.dbo.ChReg")
     end
         
   end
 
-  def send_to_clients(params = {})
-    client = TinyTds::Client.new(:username => 'phpuser', :password => 'phpuser', :host => '10.48.34.220', :database => "opendb", :port => "1433")
+  private
 
-    names = "uch1,uch2,uch3,uch4,uch5,uch6,uch7,uch8,uch9,uch10,uch11,uch12,uch13,uch14,uch15,uch16,uch17"
-    values = "''#{params[:last_name]}'',''#{params[:first_name]}'','''','''',''#{params[:city]}'','''',''#{params[:mobile]}'',''#{params[:email]}'',''1'',''0'',''1'',''0'','''',''1'',''1'',''0'',''http://localhost:3000''"
-
-    query = "exec dbo.lk_anketa_handler @anketa_type ='#{params[:anketa_type]}' ,@anketa_names ='#{names}' ,@anketa_values ='#{values}' ,@is_insert = 1"
-
-    result = client.execute(query)
-    client.close
-
-    puts result.each
-  end
+    def self.service_params
+      {
+        :id => "NEW",
+        :prospectSourceType => "Our own sales network", 
+        :"web-RequestType" => "Active Sale",
+        :lastName => "Test",
+        :firstName => "Test",
+        :middleName => "Test",
+        :alternatePhone => "+79119998877",
+        :emailAddress => "andreev-a@open.ru",
+        :comment => "Тестирование WSDL",
+        :bestCallTime => "",
+        :callbackDatetime => "",
+        :city => "Москва",
+        :comments => "Тестирование WSDL",
+        :organization => "",
+        :"web-RequestSourceURL" => "HTTP RequestSourceURL",
+        :"web-RequestHTTPReferer" => "HTTP RequestHTTPReferer",
+        :formSourceURL => "formSourceURL",
+        :"web-RequestProductName" => "Имя продукта"
+      }
+    end
 
 end
